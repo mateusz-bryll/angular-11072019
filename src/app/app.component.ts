75e1087b5dfe43909dca54aac29627a7ff2815d2
Mateusz Bryll <mateusz.bryll@outlook.com>
2019-07-07 13:03:01 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fcm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'fcm';
+}
